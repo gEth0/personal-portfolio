3b58c617b6d62bbdc91aea59186ba77720886238
gEth0 <solo.per.testing.py@gmail.com>
2022-10-06 20:44:45 +0200

    animate on scroll

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
-function responsive() {
-    var x = document.getElementById("nav-div");
-    if (x.className === "nav-div") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav-div";
-    }
+x = document.getElementById('type1')
+y = document.getElementById('type2')
+z = document.getElementById('type3')
+
+function addclass (){
+  if(document.documentElement.scrollTop > 370){
+    x.classList.remove('title-p')
+    x.classList.add('typed-out')
+  }else{
+    x.classList.add('title-p')
   }
+  if(document.documentElement.scrollTop > 1219){
+    y.classList.remove('title-p')
+    y.classList.add('typed-out')
+  }else{
+    y.classList.add('title-p')
+  }
+  if(document.documentElement.scrollTop > 2067){
+    z.classList.remove('title-p')
+    z.classList.add('typed-out')
+  }else{
+    z.classList.add('title-p')
+  }
+
+  
+}
+
+window.onscroll = function(){addclass()}
